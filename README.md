e518732a24e707fa59bc2b9be802c4dcd9197f19
Duane Jeffers <duane@duanejeffers.com>
2014-09-23 11:17:51 -0500

    Added composer.json and main source file. Updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-slim-layout
-===========
+# Slim Layout
 
-Simple Layout Class for Slim Framework
+This is a simple layout object for Slim Framework that overrides the default View Layout.
+
+## Installing
+
+In your composer.json file, make sure to require:
+`"tylerjuniorcollege/slim-layout":"dev-master"`
+
+## Using in your Application.
+
---
+++ ViewLayout.php
@@ -0,0 +1,48 @@
+<?php
+	/**
+	 * Slim ViewLayout.
+	 * - This is used to override the default View object with Slim.
+	 *
+	 * (c) Copyright 2014 Tyler Junior College
+	 * See LICENSE file for License details
+	 **/
+
+namespace TJC;
+
+use \Slim\View as SlimView;
+
+class ViewLayout extends SlimView {
+	protected $layout = null;
+	protected $layoutData = array();
+
+	public function setLayout($template) {
+		$layout = $this->getTemplatePathname($template);
+		if(!is_file($layout)) {
+			throw new \RuntimeException("Layout file `$template` does not exist.");
+		} else {
+			$this->layout = $template;
+		}
+	}
+
+	public function getLayoutData() {
+		return $this->layoutData;
+	}
+
+	public function setLayoutData($data, $value = null) {
+		if(is_array($data)) {
+			$this->layoutData = array_merge($this->layoutData, $data);
+		} elseif(!is_null($value)) {
+			$this->layoutData[$data] = $value;
+		}
+	}
+
+	public function render($template, $data = null) {
+		if(!is_null($this->layout)) { // Render the layout!!
+			$this->setLayoutData('content', parent::render($template, $data));
+
+			return parent::render($this->layout, $this->layoutData);
+		} else {
+			return parent::render($template, $data);
+		}
+	}
+}
